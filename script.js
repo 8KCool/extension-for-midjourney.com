9d2dc160c6d78443bb66f3891b3aa8e56fd94938
BlackPandalancer <8klancer@gmail.com>
2023-04-24 15:51:36 +0000

    feat: add fave event

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
 window.onload = () => {
   windowFlag = true;
-  setTimeout(showFavButton, 1000);
+  setTimeout(initFavButton, 1000);
 }
 
 // document.addEventListener("click", function(event) {
 //   if (event.target.matches(".ams.bkH")) { // reply button clicked
 //     // showGPTModal();
 //     windowFlag = true;
-//     setTimeout(showFavButton, 1000);
+//     setTimeout(initFavButton, 1000);
 //   }
 // });
 
-const showFavButton = () => {
+const initFavButton = () => {
 
   // check the user is available 
   const hotSortbutton = document.querySelectorAll('[title="Sort by Hot"]');
@@ -25,21 +25,47 @@ const showFavButton = () => {
     observer.observe(contentElement, { childList: true, subtree: true });
 
     // draw favorite button
-    const gridElements = document.querySelectorAll('[role="grid"] .relative');
+    const gridElements = document.querySelectorAll('[role="grid"] .grid.relative');
     gridElements.forEach((gridElement) => {
       const childElement = document.createElement('div');
 
       childElement.classList.add("fav-panel")
       const imageUrl = chrome.runtime.getURL('icons/32.png');
-      childElement.innerHTML = `<div class='fav-button' style='background-image:url("${imageUrl}")'></div>`
+
+      const button = document.createElement('div');
+      button.classList.add('fav-button');
+      button.setAttribute('style', `background-image:url("${imageUrl}")`);
+
+
+      button.addEventListener('click', function (event) {
+        // Replace misspelled word with suggestion
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://www.midjourney.com/api/app/user-rank/');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = function () {
+          if (xhr.status === 200) {
+            console.log(xhr.responseText);
+          } else {
+            console.error(xhr.statusText);
+          }
+        };
+        const data = {
+          jobId: "c655e4d9-43fd-4b2e-8396-477f452ef950",
+          value: 4
+        };
+        xhr.send(JSON.stringify(data));
+
+      });
+
       gridElement.appendChild(childElement);
+      childElement.appendChild(button);
     });
   }
 
 }
 
 function event_Container(mutations) {
-  const gridElements = document.querySelectorAll('[role="grid"] .relative');
+  const gridElements = document.querySelectorAll('[role="grid"] .grid.relative');
   gridElements.forEach((gridElement) => {
     const favButton = gridElement.querySelector('.fav-panel');
     if (!favButton) {
@@ -47,10 +73,15 @@ function event_Container(mutations) {
 
       childElement.classList.add("fav-panel")
       const imageUrl = chrome.runtime.getURL('icons/32.png');
-      childElement.innerHTML = `<div class='fav-button' style='background-image:url("${imageUrl}")'></div>`
+      childElement.innerHTML = `<div class='fav-button' id="headlessui-menu-item-15" style='background-image:url("${imageUrl}")'></div>`
       gridElement.appendChild(childElement);
     }
   });
+}
+
+
+const renderFavButton = () => {
+
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
