9ed4c7ecfaaef8391c4b543db5e13dc8a45bfa86
BlackPandalancer <8klancer@gmail.com>
2023-04-25 02:12:52 +0000

    feat: insert favaddevent, favremoveevent fix: make callback fav event response refactor: change code to detailed function

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
 
 window.onload = () => {
-  windowFlag = true;
   setTimeout(initFavButton, 1000);
 }
-
-// document.addEventListener("click", function(event) {
-//   if (event.target.matches(".ams.bkH")) { // reply button clicked
-//     // showGPTModal();
-//     windowFlag = true;
-//     setTimeout(initFavButton, 1000);
-//   }
-// });
 
 const initFavButton = () => {
 
   // check the user is available 
-  const hotSortbutton = document.querySelectorAll('[title="Sort by Hot"]');
-  if (hotSortbutton.length > 0) {
-
+  if (checkUserLoggedIn()) {
     // get event when the content change using ajax
     const observer = new MutationObserver(event_Container);
     const gridParent = document.querySelector('#app-root');
@@ -27,41 +16,86 @@ const initFavButton = () => {
     // draw favorite button
     const gridElements = document.querySelectorAll('[role="grid"] .grid.relative');
     gridElements.forEach((gridElement) => {
-      const childElement = document.createElement('div');
-
-      childElement.classList.add("fav-panel")
-      const imageUrl = chrome.runtime.getURL('icons/32.png');
-
-      const button = document.createElement('div');
-      button.classList.add('fav-button');
-      button.setAttribute('style', `background-image:url("${imageUrl}")`);
-
-
-      button.addEventListener('click', function (event) {
-        // Replace misspelled word with suggestion
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', 'https://www.midjourney.com/api/app/user-rank/');
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.onload = function () {
-          if (xhr.status === 200) {
-            console.log(xhr.responseText);
-          } else {
-            console.error(xhr.statusText);
-          }
-        };
-        const data = {
-          jobId: "c655e4d9-43fd-4b2e-8396-477f452ef950",
-          value: 4
-        };
-        xhr.send(JSON.stringify(data));
-
-      });
-
-      gridElement.appendChild(childElement);
-      childElement.appendChild(button);
+      renderFavButton(gridElement);
     });
   }
+}
 
+const checkUserLoggedIn = () => {
+  const hotSortbutton = document.querySelectorAll('[title="Sort by Hot"]');
+  if (hotSortbutton.length > 0)
+    return true;
+  return false;
+}
+
+const renderFavButton = (gridElement) => {
+  const childElement = document.createElement('div');
+  childElement.classList.add("fav-panel")
+  const imageUrl = chrome.runtime.getURL('icons/32.png');
+  const button = document.createElement('div');
+  button.classList.add('fav-button');
+  button.setAttribute('style', `background-image:url("${imageUrl}")`);
+  gridElement.appendChild(childElement);
+  childElement.appendChild(button);
+
+  let jobId = getJobId(gridElement);
+
+  // init add fav function
+  button.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    incrementFavImage(jobId, (result) => {
+
+    });
+
+    decrementFavImage(jobId, (result) => {
+
+    });
+  });
+}
+
+const getJobId = (gridElement) => {
+  if (!gridElement) return "";
+  const jobImgTag = gridElement.querySelector(".aspect-auto.rendering-contrast");
+  if (jobImgTag) {
+    return jobImgTag.getAttribute("data-job-id");
+  }
+
+  return "";
+}
+
+const incrementFavImage = (jobId, callback) => {
+  var url = `https://www.midjourney.com/api/app/user-like/?jobId=${jobId}&value=increment`;
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var response = JSON.parse(xhr.responseText);
+      console.log(response);
+      callback(response)
+    } else {
+      console.log(xhr.statusText);
+      callback("error");
+    }
+  }
+  xhr.open("GET", url);
+  xhr.send(null);
+}
+
+const decrementFavImage = (jobId, callback) => {
+  var url = `https://www.midjourney.com/api/app/user-like/?jobId=${jobId}&value=decrement`;
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var response = JSON.parse(xhr.responseText);
+      console.log(response);
+      callback(response)
+    } else {
+      console.log(xhr.statusText);
+      callback("error");
+    }
+  }
+  xhr.open("GET", url);
+  xhr.send(null);
 }
 
 function event_Container(mutations) {
@@ -77,11 +111,6 @@ function event_Container(mutations) {
       gridElement.appendChild(childElement);
     }
   });
-}
-
-
-const renderFavButton = () => {
-
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
